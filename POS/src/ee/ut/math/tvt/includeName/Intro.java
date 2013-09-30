664aac77fbc8b0478eefd05df04e925c039cdc78
HansP <soulyyy@gmail.com>
2013-09-30 18:15:24 +0300

    initial commit

---
+++ POS/src/ee/ut/math/tvt/includeName/Intro.java
@@ -0,0 +1,9 @@
+package ee.ut.math.tvt.includeName;
+
+public class Intro {
+
+	
+	public static void main(String[] args){
+		
+	}
+}
---
+++ POS/src/ee/ut/math/tvt/includeName/IntroUI.java
@@ -0,0 +1,5 @@
+package ee.ut.math.tvt.includeName;
+
+public class IntroUI {
+
+}
