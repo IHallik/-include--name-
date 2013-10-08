29f07636c2f9675600ae1e5fa169bb3206dca7da
Meelis <murka10@gmail.com>
2013-10-08 19:10:46 +0300

    Added TODO about logging Build script includes *.properties files in .jar

--- POS/src/ee/ut/math/tvt/includeName/Intro.java
+++ POS/src/ee/ut/math/tvt/includeName/Intro.java
@@ -3,6 +3,11 @@ package ee.ut.math.tvt.includeName;
 public class Intro {
 	
 	public static void main(String[] args){
+		/* TODO: In addition, you need to add corresponding message
+		* in the log that would indicate that intro window is opened.
+		* Use Log4J functionality for it. Log should have event time
+		* and name of the component (invocated class name)
+		*/
 		System.out.println("A miks sa oled siis aplikatsioon?");
 	}
 }
