b0357f7bb79c66e07b1bc0c47d42e6c93e7b1f80
Meelis <meelis.myyr@gmail.com>
2013-10-13 23:39:15 +0300

    Removed TODO note

--- POS/src/ee/ut/math/tvt/includeName/Intro.java
+++ POS/src/ee/ut/math/tvt/includeName/Intro.java
@@ -6,11 +6,6 @@ public class Intro {
 	private static final Logger log = Logger.getLogger(Intro.class);
 	
 	public static void main(String[] args){
-		/* TODO: In addition, you need to add corresponding message
-		* in the log that would indicate that intro window is opened.
-		* Use Log4J functionality for it. Log should have event time
-		* and name of the component (invocated class name)
-		*/
 		log.info("Intro window opened");
 		IntroUI ui = new IntroUI();
 		ui.display();
