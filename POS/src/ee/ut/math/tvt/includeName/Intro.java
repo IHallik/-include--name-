afbd211d0d25cb033d94a2d9786f514d1f06dae7
Meelis <meelis.myyr@gmail.com>
2013-10-14 15:19:36 +0300

    Log4j config file is in /etc in jar and added property to load from it

--- POS/src/ee/ut/math/tvt/includeName/Intro.java
+++ POS/src/ee/ut/math/tvt/includeName/Intro.java
@@ -2,10 +2,12 @@ package ee.ut.math.tvt.includeName;
 
 import org.apache.log4j.Logger;
 
-public class Intro {
-	private static final Logger log = Logger.getLogger(Intro.class);
-	
+public class Intro {	
 	public static void main(String[] args){
+		//Read log4j config from etc folder
+		System.setProperty("log4j.configuration", Intro.class.getResource("/etc/log4j.xml").toString());
+		Logger log = Logger.getLogger(Intro.class);
+		
 		log.info("Intro window opened");
 		IntroUI ui = new IntroUI();
 		ui.display();
